57f8bdab0db5966eb4d888845a1e4759d53ceb94
mauricio <mmauriciosanches@hotmail.com>
2022-01-11 14:55:32 -0300

    Add - adicionadas novas migrations

---
+++ database/migrations/2022_01_10_185135_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    
+    public function up()
+    {
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropColumn(['uf', 'email']);
+        });
+    }
+}
---
+++ database/migrations/2022_01_10_192216_create_produto_detalhes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produto_id');
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->unique('produto_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
