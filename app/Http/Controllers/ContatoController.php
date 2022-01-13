f8295c86616f9066cf65aed2ebcf7cbcb5248bea
mauricio <mmauriciosanches@hotmail.com>
2022-01-12 23:39:26 -0300

    melhorias na validação do formulário e correção no nome da coluna

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\MotivoContato;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
@@ -14,16 +15,26 @@ class ContatoController extends Controller
         return view('site.contato', compact('motivo_contatos'));
     }
 
-    public function salvar(Request $request){
-
-        $request->validate([
+    public function salvar(Request $request) {
+        $regras =             [
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
 
-        // SiteContato::create($request->all());
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'email.email' => 'O email informado não é válido',
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,28 +1,35 @@
-<form action="{{route('site.contato')}}" method="post">
+<form action={{ route('site.contato') }} method="post">
     @csrf
     
-    <input type="text" value="{{ old('nome') }}" name="nome" placeholder="Nome" class="{{ $classe }}">
+    <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+        @if ($errors->has('nome'))
+        <b style="font-size: 12px; color:red;">{{ $errors->first('nome') }}</b>
+        @endif
     <br>
 
-    <input type="text" value="{{ old('telefone') }}" name="telefone" placeholder="Telefone" class="{{ $classe }}">
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    <b style="font-size: 12px; color:red;">{{ $errors->has('telefone') ? $errors->first('telefone') : '' }}</b>
 
     <br>
 
-    <input type="text" value="{{ old('email') }}" name="email" placeholder="E-mail" class="{{ $classe }}">
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    <b style="font-size: 12px; color:red;">{{ $errors->has('email') ? $errors->first('email') : '' }}</b>
+
     <br>
 
-    <select name="motivo_contato" class="{{ $classe }}">
+    <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
-
-        @foreach ($motivo_contatos as $key => $mc)
-            <option value="{{ $mc->id }}" {{ old('motivo_contato') == $mc->id ? 'selected' : '' }}>{{ $mc->motivo_contato }}</option>
-        @endforeach 
-
+        @foreach($motivo_contatos as $key => $motivo_contato)
+            <option value="{{$motivo_contato->id}}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}>{{$motivo_contato->motivo_contato}}</option>
+        @endforeach
     </select>
 
+    <b style="font-size: 12px; color:red;">{{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}</b>
+
     <br>
-    
-    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui sua mensagem' }}</textarea>
+
+    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
+    <b style="font-size: 12px; color:red;">{{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}</b>
 
     <br>
 
